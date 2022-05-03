5ca6820087effd55cc2fae13a953340bc5637b43
无声 <ivestszheng@qq.com>
2022-05-03 16:02:30 +0800

    refactor(treeInSelect): 实现选择父节点模式的删除的视图逻辑

--- src/components/TreeInSelect/index.vue
+++ src/components/TreeInSelect/index.vue
@@ -1,7 +1,37 @@
 <template>
   <div class="content-box">
     <div class="container">
+      <!-- 可以单独选中父节点 -->
       <el-select
+        v-if="chooseParentNode"
+        v-model="selectTree"
+        clearable
+        placeholder="请选择"
+        :popper-append-to-body="false"
+        multiple
+        class="select-tree"
+        @clear="handleClear"
+        @remove-tag="handleRemoveTag"
+      >
+        <el-option :value="selectTreeValue" style="height: auto">
+          <el-tree
+            ref="tree"
+            :data="data"
+            show-checkbox
+            node-key="id"
+            accordion
+            highlight-current
+            icon-class="el-icon-arrow-down"
+            :default-expand-all="true"
+            :check-strictly="true"
+            @check="handleCheck"
+          ></el-tree>
+        </el-option>
+      </el-select>
+
+      <!-- 不可以单独选中父节点 -->
+      <el-select
+        v-else
         v-model="selectTree"
         clearable
         placeholder="请选择"
@@ -11,9 +41,7 @@
         @change="selectChange"
       >
         <el-option :value="selectTreeValue" style="height: auto">
-          <!-- 可以单独选中父节点 -->
           <el-tree
-            v-if="chooseParentNode"
             ref="tree"
             :data="data"
             show-checkbox
@@ -21,25 +49,15 @@
             accordion
             highlight-current
             icon-class="el-icon-arrow-down"
-            :check-strictly="true"
-            @check="handleCheck"
-          ></el-tree>
-          <!-- 不可以单独选中父节点 -->
-          <el-tree
-            v-else
-            ref="tree"
-            :data="data"
-            show-checkbox
-            node-key="id"
-            accordion
-            highlight-current
-            icon-class="el-icon-arrow-down"
+            :default-expand-all="true"
             @check-change="handleCheckChange"
           ></el-tree>
         </el-option>
       </el-select>
-      <div>{{ selectTree }}</div>
-      <div>{{ selectTreeValue }}</div>
+      <div>长度：{{ selectTree.length }}</div>
+      <div>内容：{{ selectTree }}</div>
+      <div>长度：{{ selectTreeValue.length }}</div>
+      <div>内容：{{ selectTreeValue }}</div>
     </div>
   </div>
 </template>
@@ -59,25 +77,65 @@ export default {
   },
   data() {
     return {
-      selectTree: '',
+      selectTree: [],
       selectTreeValue: [],
     };
   },
   methods: {
+    handleClear() {
+      console.log('清空');
+
+      const vitrualNodes = this.$refs.tree.getCheckedNodes(false, false);
+
+      this.selectTreeValue = [];
+      vitrualNodes.forEach((vitrualNode) => {
+        const actualNode = this.$refs.tree.getNode(vitrualNode.id);
+        actualNode.data.mark = 0;
+        actualNode.checked = false;
+      });
+    },
     // select框值改变时候触发的事件
     selectChange(e) {
-      console.log('select change', e);
+      console.log('默认模式 select change', e);
+      const eLen = e.length;
       const arrNew = [];
       const dataLength = this.selectTreeValue.length;
-      const eleng = e.length;
+
       for (let i = 0; i < dataLength; i += 1) {
-        for (let j = 0; j < eleng; j += 1) {
+        for (let j = 0; j < eLen; j += 1) {
           if (e[j] === this.selectTreeValue[i].label) {
             arrNew.push(this.selectTreeValue[i]);
           }
         }
       }
       this.$refs.tree.setCheckedNodes(arrNew); // 设置勾选的值
+    },
+    // 多选模式下移除tag时触发
+    handleRemoveTag(tag) {
+      console.log('单独删除', tag);
+      // 触发这个事件时候代表删除动作，且节点里一定有 mark 属性
+      // selectTree 不需要手动维护
+      // 只需要维护 selectTreeValue
+      for (let index = 0; index < this.selectTreeValue.length; index += 1) {
+        const { label } = this.selectTreeValue[index];
+        if (tag === label) {
+          this.selectTreeValue.splice(index, 1);
+        }
+      }
+      const vitrualNodes = this.$refs.tree.getCheckedNodes(false, false);
+
+      for (let i = 0; i < vitrualNodes.length; i += 1) {
+        const vitrualNode = vitrualNodes[i];
+
+        if (tag === vitrualNode.label) {
+          const actualNode = this.$refs.tree.getNode(vitrualNode.id);
+
+          actualNode.data.mark = 0;
+          actualNode.checked = false;
+          actualNode.indeterminate = false;
+          break;
+        }
+      }
     },
     // 节点选中状态发生变化时的回调
     handleCheckChange() {
@@ -94,8 +152,8 @@ export default {
     // 当复选框被点击的时候触发
     // 共两个参数，依次为：传递给 data 属性的数组中该节点所对应的对象、树目前的选中状态对象，包含 checkedNodes、checkedKeys、halfCheckedNodes、halfCheckedKeys 四个属性
     handleCheck(currentObj, allSelected) {
-      // console.log('currentObj', currentObj);
-      // console.log('allSelected', allSelected);
+      console.log('currentObj', currentObj);
+      console.log('allSelected', allSelected);
 
       // 这个方法不会再次触发 check 事件
       // this.$refs.tree.setChecked(3, true);
@@ -104,12 +162,31 @@ export default {
     },
     judgeMark(currentObj = {}) {
       /* eslint-disable no-param-reassign */
-      const changeChildrenStatus = (children) => {
+
+      const removeNode = (obj) => {
+        for (let index = 0; index < this.selectTree.length; index += 1) {
+          const ele = this.selectTree[index];
+          if (ele === obj.label) {
+            this.selectTree.splice(index, 1);
+            break;
+          }
+        }
+        for (let index = 0; index < this.selectTreeValue.length; index += 1) {
+          const { label } = this.selectTreeValue[index];
+          if (label === obj.label) {
+            this.selectTreeValue.splice(index, 1);
+            break;
+          }
+        }
+      };
+
+      const changeChildrenStatusToFalse = (children) => {
         children.forEach((child) => {
           child.mark = 0;
           this.$refs.tree.setChecked(child.id, false);
+          removeNode(child);
           if (Object.prototype.hasOwnProperty.call(child, 'children')) {
-            changeChildrenStatus(child.children);
+            changeChildrenStatusToFalse(child.children);
           }
         });
       };
@@ -121,8 +198,8 @@ export default {
             writable: true,
           });
           this.$refs.tree.setChecked(currentObj.id, true);
-          // this.selectTreeValue.push(currentObj);
-          // this.selectTree.push(currentObj.label);
+          this.selectTreeValue.push(currentObj);
+          this.selectTree.push(currentObj.label);
 
           if (Object.prototype.hasOwnProperty.call(currentObj, 'children')) {
             currentObj.children.forEach((child) => {
@@ -133,8 +210,8 @@ export default {
         case 0:
           currentObj.mark = 1;
           this.$refs.tree.setChecked(currentObj.id, true);
-          // this.selectTreeValue.push(currentObj);
-          // this.selectTree.push(currentObj.label);
+          this.selectTreeValue.push(currentObj);
+          this.selectTree.push(currentObj.label);
 
           if (Object.prototype.hasOwnProperty.call(currentObj, 'children')) {
             currentObj.children.forEach((child) => {
@@ -146,14 +223,16 @@ export default {
           if (Object.prototype.hasOwnProperty.call(currentObj, 'children')) {
             currentObj.mark = 2;
             this.$refs.tree.setChecked(currentObj.id, true);
-            changeChildrenStatus(currentObj.children);
+            changeChildrenStatusToFalse(currentObj.children);
           } else {
             currentObj.mark = 0;
             this.$refs.tree.setChecked(currentObj.id, false);
+            removeNode(currentObj);
           }
           break;
         case 2:
           currentObj.mark = 0;
+          removeNode(currentObj);
           break;
         default:
           break;
