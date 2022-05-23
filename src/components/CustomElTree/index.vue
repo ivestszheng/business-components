7f5617e877355d1bcac937ce23e0dcaf8e0a9cd7
无声 <ivestszheng@qq.com>
2022-05-23 16:39:18 +0800

    fix(ELTree): 修复同级拖拽模式下拖拽手势一直是禁止的问题

--- src/components/CustomElTree/index.vue
+++ src/components/CustomElTree/index.vue
@@ -8,8 +8,7 @@
     :props="props"
     :allow-drop="allowDrop"
     :allow-drag="allowDrag"
-    @node-drag-start="handleDragStart"
-    @node-drag-end="handleDragEnd"
+    @node-drag-over="handleDragOver"
     @node-drop="handleDrop"
   >
     <span slot-scope="{ data }" class="custom-tree-node">
@@ -39,13 +38,7 @@ export default {
     },
   },
   methods: {
-    handleDragStart(node, ev) {
-      console.log('drag start', node);
-    },
-    handleDragEnd(draggingNode, dropNode, dropType, ev) {
-      console.log('tree drag end: ', dropNode && dropNode.label, dropType);
-    },
-    handleDrop(draggingNode, dropNode, dropType, ev) {
+    handleDrop(draggingNode, dropNode, dropType, event) {
       console.log('tree drop: ', dropNode.label, dropType);
     },
     allowDrop(draggingNode, dropNode, type) {
@@ -54,6 +47,27 @@ export default {
     },
     allowDrag(draggingNode) {
       return true;
+    },
+    /**
+     * handleDragOver 触发频率很高
+     * 按照我的理解，本来应该通过 enter 和 leave 事件来修改手势的，以提高性能
+     * 但是由于 allowDrop 的存在，导致 enter 和 leave 事件不能像预期那样触发
+     * 所以只能使用 over 事件
+     */
+    handleDragOver(draggingNode, dropNode, event) {
+      const isAbleToDrop = draggingNode.parent === dropNode.parent;
+      if (this.sameLevelDrop && !isAbleToDrop) {
+        console.log('不允许 drop');
+      } else if (this.sameLevelDrop && isAbleToDrop) {
+        console.log('允许 drop');
+        /**
+         * 不能使用 setData api
+         * 这样没有效果
+         * event.dataTransfer.setData('dropEffect', 'move')
+         */
+        // eslint-disable-next-line no-param-reassign
+        event.dataTransfer.dropEffect = 'move';
+      }
     },
   },
 };
